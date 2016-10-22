ac76f03281f816af3b3041ea2f23b5c13c5e53f2
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-22 00:21:43 -0700

    exporting script as a function to run in express server

--- scripts/crashDataConversion.js
+++ scripts/crashDataConversion.js
@@ -2,35 +2,39 @@
 
 var bookshelf = require('../bookshelf')
 
-
-
-// knex.schema.createTableIfNotExists("plane_crashes_1908", function(table) {
+module.exports = function(knex) {
   
-//   table.increments('crash_id').primary();
-//   table.string('date')
-//   table.string('time')
-//   table.string('location')
-//   table.string('operator')
-//   table.string('flight_#')
-//   table.string('route')
-//   table.string('type')
-//   table.string('registration')
-//   table.integer('aboard')
-//   table.integer('fatalities')
-//   table.integer('ground')
-//   table.string('summary')
-//   console.log('table created')
+  knex.schema.createTableIfNotExists("plane_crashes_1908", function(table) {
+    
+    table.increments('crash_id').primary();
+    table.string('date')
+    table.string('time')
+    table.string('location')
+    table.string('operator')
+    table.string('flight_#')
+    table.string('route')
+    table.string('type')
+    table.string('registration')
+    table.integer('aboard')
+    table.integer('fatalities')
+    table.integer('ground')
+    table.string('summary')
+  }).then(function(){ console.log('done')})
 
-// })
+  var User = bookshelf.Model.extend({
+    tableName: 'users'
+  })
 
-var users = bookshelf.Model.extend({
-  tableName: 'users'
-})
+  new User({name: 'New User'}).save().then(function(){
+    console.log('saved a user')
+  })
 
-//Converter Class 
-const Converter = require("csvtojson").Converter;
-const converter = new Converter({});
+  //Converter Class 
+  const Converter = require("csvtojson").Converter;
+  const converter = new Converter({});
 
-converter.fromFile("/Users/tonywinglau/Desktop/Datasets/Airplane_Crashes_and_Fatalities_since_1908.csv", function(err, result) {
- console.log(result[0])
-});
+  converter.fromFile("/Users/tonywinglau/Desktop/Datasets/Airplane_Crashes_and_Fatalities_since_1908.csv", function(err, result) {
+   console.log(result[0])
+  });
+}
+
