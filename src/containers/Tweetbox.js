97ead3fd3ea7e0fe48d4567a19f20424a1b1b884
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-22 20:47:37 -0700

    Refactoring Tweetbox into a container

---
+++ src/containers/Tweetbox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import CSS from '../components/AppStyles'
+
+export default class TweetBox extends Component {
+  render(){
+    return (
+      <aside className="aside aside-2" style={CSS.aside2}>
+        
+      </aside>
+    )
+  }
+}
