15cfc97a8cdab716a2d37d181ec5266d86766f73
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-22 22:38:47 -0700

    connected Tweetbox to redux application state

--- src/containers/Tweetbox.js
+++ src/containers/Tweetbox.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { fetchTweets } from '../../actions/actions'
 import CSS from '../components/AppStyles'
 
-export default class TweetBox extends Component {
-  render(){
+class TweetBox extends Component {
+
+  componentWillMount() {
+    this.props.dispatch(fetchTweets())
+  }
+
+  renderTweet(tweet) {
+    return (
+      <tr key={tweet.id} className="tweet">
+        <td>{tweet.text}</td>
+      </tr>
+    )
+  }
+
+  render() {
+    console.log('my current props', this.props)
     return (
       <aside className="aside aside-2" style={CSS.aside2}>
-        
+        <table>
+          <tbody>
+            <tr><th>tweets</th></tr>
+       
+          </tbody>
+        </table>
       </aside>
     )
   }
 }
+
+function mapStateToProps(state) {
+  console.log(state)
+  return { username: state.username, tweets: state.tweets}
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchTweets }, dispatch)
+// }
+
+export default connect(mapStateToProps)(TweetBox)
