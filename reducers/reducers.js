6d5d5800da0f63de4a81ff1033963c091edd565c
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-22 22:36:58 -0700

    added tweet reducer to combineReducer Call

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { greeting } from './reducerGreeting'
+import { tweets } from './reducerTweets'
 
 export default combineReducers({
+  tweets,
   greeting
 })
