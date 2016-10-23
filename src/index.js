611dc554ddf36516c410cf44021714aeff5553a7
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-23 16:54:52 -0700

    sucessfully added Redux_DEVTOOLS compatability

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { browserHistory } from 'react-router'
 import ReduxPromise from 'redux-promise'
 
 import Root from './root'
 import rootReducer from '../reducers/reducers'
 
-const store = createStore(rootReducer, applyMiddleware(ReduxPromise))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(ReduxPromise)
+  ))
 const history = browserHistory
 
 render(
