30047234915295a3e0272d72131d9463410945f3
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-22 20:45:34 -0700

    added fetchTweets action

--- actions/actions.js
+++ actions/actions.js
@@ -1,14 +1,27 @@
-export const GET_HELLO_WORLD = 'GET_HELLO_WORLD',
-             GET_GOODBYE_WORLD = 'GET_GOODBYE_WORLD'
+import axios from 'axios'
+
+export const GET_HELLO_WORLD   = 'GET_HELLO_WORLD',
+             GET_GOODBYE_WORLD = 'GET_GOODBYE_WORLD',
+             FETCH_TWEETS      = 'FETCH_TWEETS'
 
 export function getHelloWorld() {
   return {
-    type: GET_HELLO_WORLD,
+    type: GET_HELLO_WORLD
   }
 }
 
 export function getGoodbyeWorld() {
   return {
-    type: GET_GOODBYE_WORLD,
+    type: GET_GOODBYE_WORLD
+  }
+}
+
+export function fetchTweets(params = 'thestylisted') {
+  const URL = 'localhost:3000/tweets/' + params.screen_name
+  const tweets = axios.get(URL)
+
+  return {
+    type: FETCH_TWEETS,
+    payload: tweets
   }
 }
