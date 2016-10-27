358fffe347d0c10ad0a3aee84a53a2ebbab465d2
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-26 21:51:05 -0700

    implemented a date formatting method formatDates() which returns a momentJs object

--- src/containers/Tweetbox.js
+++ src/containers/Tweetbox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import moment from 'moment'
 
 import { fetchTweets } from '../../actions/actions'
 import CSS from '../components/AppStyles'
@@ -24,21 +25,26 @@ class TweetBox extends Component {
     //create an array of sorted tweet id's (Chronologic order)
     var sortedIndexKeys = Object.keys(tweetsHash).sort(function(a, b) { return b - a })
 
+    
+
+
     //create an array of tweets
-    var sortedTweets = sortedIndexKeys.map(function(indexKey){
-      return {id: indexKey, status: tweetsHash[indexKey].text}
+    var sortedTweets = sortedIndexKeys.map(function(indexKey) {
+      return { id: indexKey, status: tweetsHash[indexKey].text, createdAt: tweetsHash[indexKey].created_at }
     })
     //return jsx elements
     return sortedTweets.map(function(tweet){
       return (
         <div key={tweet.id} className="tweet" style={CSS.tweet}>
           {tweet.status}
+          <h6>{formatDate(tweet.createdAt).fromNow()}</h6>
         </div>
       )
     })
   }
 
   render() {
+
     return (
       <aside className="aside aside-2" style={CSS.aside2}>
         <h2>{this.props.username}</h2>
@@ -54,6 +60,38 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchTweets }, dispatch)
+}
+
+/**
+*@params {string} dateString - a date from twitter formatted "Wed Oct 26 18:31:56 +0000 2016"
+*@returns {string} a date formatted to "YYYY-MM-DD HH:mm:ss"  
+*/
+function formatDate(dateString) {
+
+  const months = {
+    Jan: 1,
+    Feb: 2,
+    Mar: 3,
+    Apr: 4,
+    May: 5,
+    Jun: 6,
+    Jul: 7,
+    Aug: 8,
+    Sep: 9,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12
+  }
+
+  console.log("this is datestring", dateString)
+
+  var result = [],
+      temp
+  temp = dateString.split(' ')
+  result.push(temp[temp.length -1], temp[1], temp[2])
+  result[1] = months[result[1]]
+  result = result.join('-') + " " + temp[3]
+  return moment(result)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TweetBox)
