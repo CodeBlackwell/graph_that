04f7f273fa0197f2776774a8324859f44fc967bd
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-31 11:07:06 -0700

    created fetchPlaneCrashData()

--- actions/actions.js
+++ actions/actions.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export const GET_HELLO_WORLD   = 'GET_HELLO_WORLD',
              GET_GOODBYE_WORLD = 'GET_GOODBYE_WORLD',
-             FETCH_TWEETS      = 'FETCH_TWEETS'
+             FETCH_TWEETS      = 'FETCH_TWEETS',
+             FETCH_PLANE_CRASH_DATA = 'FETCH_PLANE_CRASH_DATA'
 
 export function getHelloWorld() {
   return {
@@ -19,14 +20,20 @@ export function getGoodbyeWorld() {
 export function fetchTweets(username = 'thestylisted', oldestTweetId) {
   oldestTweetId = oldestTweetId || ''
   const URL = 'http://localhost:3000/tweets/' + username
-
-  console.log('this is the oldest tweet id within fetchTweets', oldestTweetId)
-
   const params = { max_id: oldestTweetId}
 
   const tweets = axios.get(URL, {params: params})
   return {
     type: FETCH_TWEETS,
     payload: tweets
+  }
+}
+
+export function fetchCrashData() {
+  const URL = 'http://localhost:3000/data/planecrashes'
+  const crashData = axios.get(URL)
+  return {
+    type: FETCH_PLANE_CRASH_DATA,
+    payload: crashData
   }
 }
